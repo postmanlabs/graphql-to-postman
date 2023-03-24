d46f28c08e543034a88f1f2b197af087a5695ef4
Vishal Shingala <vishalb.shingala@gmail.com>
2023-03-24 19:17:23 +0530

    Fixed issue where conversion failed with typeeror while resolving non-defined variables

---
+++ test/unit/gql-generator.test.js
@@ -0,0 +1,25 @@
+const schemaToQuery = require('../../lib/assets/gql-generator').schemaToQuery,
+  fs = require('fs'),
+  path = require('path'),
+  graphql = require('graphql'),
+  validSchemaSDL = fs.readFileSync(path.join(__dirname, './fixtures/validSchemaSDL.graphql')).toString(),
+  expect = require('chai').expect;
+
+describe('gql-generator tests', function () {
+  it('should not throw type error for non defined elements in GQL schema', function (done) {
+    const data = validSchemaSDL,
+      gqlSchemaObj = graphql.buildSchema(data);
+
+    // Set specific property as null to test behaviour for non defined nodes.
+    gqlSchemaObj._typeMap.PatchSize = undefined;
+
+    try {
+      schemaToQuery(gqlSchemaObj, { depth: 5 });
+    }
+    catch (e) {
+      expect(e).to.be.undefined;
+    }
+
+    done();
+  });
+});
